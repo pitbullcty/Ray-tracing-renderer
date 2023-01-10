b5c8d059b939d727071ba871632b69c849024939
pitbull <1354863945@qq.com>
2023-01-10 10:55:41 +0800

    添加三方库

--- cmake-modules/Findassimp.cmake
+++ cmake-modules/Findassimp.cmake
@@ -1,17 +1,17 @@
-
-set(ASSIMP_ROOT_DIR "D:/assimp-master/build")
+﻿
+set(ASSIMP_ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/thridparty/assimp")
 # Find path of each library
 find_path(ASSIMP_INCLUDE_DIR
 	NAMES
 		assimp/anim.h
 	HINTS
-		${ASSIMP_ROOT_DIR}/../include
+		${ASSIMP_ROOT_DIR}/include
 )
 
 set(ASSIMP_MSVC_VERSION "vc143")
 
-find_library(ASSIMP_LIBRARY_RELEASE				assimp-${ASSIMP_MSVC_VERSION}-mt.lib			PATHS ${ASSIMP_ROOT_DIR}/lib/Release)
-find_library(ASSIMP_LIBRARY_DEBUG				assimp-${ASSIMP_MSVC_VERSION}-mtd.lib			PATHS ${ASSIMP_ROOT_DIR}/lib/Debug)
+find_library(ASSIMP_LIBRARY_RELEASE				assimp-${ASSIMP_MSVC_VERSION}-mt.lib			PATHS ${ASSIMP_ROOT_DIR}/lib)
+find_library(ASSIMP_LIBRARY_DEBUG				assimp-${ASSIMP_MSVC_VERSION}-mtd.lib			PATHS ${ASSIMP_ROOT_DIR}/lib)
 		
 
 		
