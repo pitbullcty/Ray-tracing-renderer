7bf912cb37785c33aedafd2a046e6b46db898637
pitbull <1354863945@qq.com>
2023-02-15 18:17:32 +0800

    新增翻译

--- src/RayTracingRenderer.cpp
+++ src/RayTracingRenderer.cpp
@@ -1,11 +1,20 @@
 ﻿#include "mainwindow.h"
 #include <QApplication>
-#include <iostream>
+#include <QTranslator>
 
 int main(int argc, char* argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts); //一定要全局共享上下文
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts); //全局共享OpenGL上下文
     QApplication a(argc, argv);
+
+    QTranslator basetranslator;
+    basetranslator.load(":/qtbase_zh_CN.qm");
+    qApp->installTranslator(&basetranslator);
+  
+    QTranslator translator;
+    qApp->installTranslator(&translator);
+    translator.load(":/qtbase_zh_CN.qm");
+
     MainWindow w;
     return a.exec();
 }   
